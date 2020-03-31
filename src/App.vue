abe0b1bcdfa16d42e3333669651ef331873560b2
Matthias Fischer <m@tthias.space>
2020-03-31 21:28:44 +0200

    add basic wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <main class="wrapper">
+        <nav class="navigation">
+          <section class="container">
+            <a class="navigation-title" href="https://ma.tthias.space" title="just my homepage :)">
+              <h1 class="title">ma.tthias.space</h1>
+            </a>
+            <ul class="navigation-list float-right">
+              <li class="navigation-item">
+                <a class="navigation-link" href="#">Github</a>
+              </li>
+            </ul>
+          </section>
+        </nav>
+
+        <home/>
+        
+      </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .navigation {
+    background:#f4f5f6;
+    border-bottom:.1rem solid #d1d1d1;
+    display:block;
+    height:5.2rem;
+    left:0;
+    max-width:100%;
+    position:fixed;
+    right:0;
+    top:0;
+    width:100%;
+    z-index:1
+  }
+
+  .navigation .container {
+    padding-bottom:0;
+    padding-top:0
+  }
+
+  .navigation .navigation-list {
+    list-style:none;
+    margin-bottom:0;
+    margin-right:5rem
+  }
+
+  @media(min-width:80rem) {
+    .navigation .navigation-list {
+      margin-right:0
+    }
+  }
+
+  .navigation .navigation-item {
+    float:left;
+    margin-bottom:0;
+    margin-left:2.5rem;
+    position:relative
+  }
+
+  .navigation .navigation-title,
+  .navigation .title {
+    color:#606c76;
+    font-family:Gotham Rounded A,Gotham Rounded B,Helvetica Neue,Arial,sans-serif;
+    position:relative
+  }
+
+  .navigation .navigation-link,
+  .navigation .navigation-title,
+  .navigation .title {
+    display:inline;
+    font-size:1.6rem;
+    line-height:5.2rem;
+    padding:0;
+    text-decoration:none
+  }
+
+  .navigation .navigation-link.active {
+    color:#606c76
+  }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <header id="home" class="header">
+        <section class="container">
+            <h1>Hello I'm Matthias</h1>
+            <p>a software development enthusiast since the age of 13 years.</p>
+        </section>
+    </header>
+</template>
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+  .header {
+    background-color:#f4f5f6;
+    padding-top:1rem
+  }
+  
+  @media(min-width:40rem) {
+    .header {
+      padding-top:5rem
+    }
+  }
+
+  .header+section {
+    border-top:0
+  }
+
+  .header .container {
+    border-top:0;
+    padding-bottom:7.5rem;
+    padding-top:7.5rem;
+    position:relative;
+    text-align:center
+  }
+
+  .header .title {
+    font-family:Gotham Rounded A,Gotham Rounded B,Helvetica Neue,Arial,sans-serif
+  }
+</style>
