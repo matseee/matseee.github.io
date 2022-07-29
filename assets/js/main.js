2caeff4971a608b0115e8e6526f99ae645615741
Matthias Fischer <m@tthias.space>
2022-07-29 23:08:33 +0200

    refactor stylesheet & add light-mode

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+(function() {
+
+    // ScrollProgress bar
+    const scrollProgress = document.getElementById('scroll-progress-bar');
+
+    window.addEventListener('scroll', () => {
+      const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  
+      scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+    });
+
+    // Toggle dark-light mode
+    const darkLightToggleImage = document.getElementById('darkLightToggle');
+
+    if (!window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.toggle('light');
+      darkLightToggleImage.src = '/assets/dark.svg';
+    }
+  
+    darkLightToggleImage.addEventListener('click', () => {
+      document.body.classList.toggle('light');
+  
+      if (darkLightToggleImage.src.indexOf('/assets/light.svg') > -1 ) {
+        darkLightToggleImage.src = '/assets/dark.svg';
+      } else {
+        darkLightToggleImage.src = '/assets/light.svg';
+      }
+    });
+
+})();
